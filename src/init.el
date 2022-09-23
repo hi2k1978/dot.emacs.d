b828113533c5c8d1b4d514fbfea2120e6292faf1
hi2k1978 <hi2k1978@gmail.com>
2022-09-24 00:01:16 +0900

    remove typescript-mode from init.el because web-mode is already installed.

--- src/init.el
+++ src/init.el
@@ -225,11 +225,11 @@
   :config
   (beacon-mode 1))
 
-(leaf typescript-mode
-  :ensure t
-  :custom
-  (typescript-indent-level . 2)
-  )
+;; (leaf typescript-mode
+;;   :ensure t
+;;   :custom
+;;   (typescript-indent-level . 2)
+;;   )
 
 (leaf rainbow-mode
   :ensure t
