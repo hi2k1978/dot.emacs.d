f368e74aa9154e631d14dc1a18c70af2b804fddb
hi2k1978 <hi2k1978@gmail.com>
2022-09-25 04:03:01 +0900

    modify emacs-configue.sh

--- build/emacs-configure.sh
+++ build/emacs-configure.sh
@@ -5,9 +5,17 @@ echo "PREFIX = ${PREFIX}"
 OPTIONS=""
 OPTIONS="${OPTIONS} --with-xwidgets"
 OPTIONS="${OPTIONS} --with-native-compilation"
+OPTIONS="${OPTIONS} --with-x-toolkit=gtk3"
 OPTIONS="${OPTIONS} --without-xim"
 echo "OPTIONS = ${OPTIONS}"
 
+sudo apt build-dep -y emacs
+sudo apt install -y \
+     libjansson4 libjansson-dev gnutls-bin libgif-dev libxpm-dev \
+     gnutls-dev libtinfo-dev libncurses-dev
+sudo apt install -y libgtk-3-dev # gtk-3
+sudo apt install -y libwebkit2gtk-4.0-dev # webkit
+sudo apt install -y libgccjit0 libgccjit-11-dev # native-compile
 configure="./configure --prefix=${PREFIX} ${OPTIONS}"
 echo $configure
 eval $configure
