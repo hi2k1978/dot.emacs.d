1771f197e69b3b776254d80a538b533b6c01e16e
hi2k1978 <hi2k1978@gmail.com>
2023-09-24 01:09:43 +0900

    modify build

--- build/emacs-configure.sh
+++ build/emacs-configure.sh
@@ -3,19 +3,29 @@ PREFIX="/usr/local"
 echo "PREFIX = ${PREFIX}"
 
 OPTIONS=""
-OPTIONS="${OPTIONS} --with-xwidgets"
 OPTIONS="${OPTIONS} --with-native-compilation"
 OPTIONS="${OPTIONS} --with-x-toolkit=gtk3"
 OPTIONS="${OPTIONS} --without-xim"
+# OPTIONS="${OPTIONS} --with-imagemagick"
+OPTIONS="${OPTIONS} --with-xft"
+OPTIONS="${OPTIONS} --with-xwidgets"
 echo "OPTIONS = ${OPTIONS}"
 
-sudo apt build-dep -y emacs
+# sudo apt build-dep -y emacs
 sudo apt install -y \
-     libjansson4 libjansson-dev gnutls-bin libgif-dev libxpm-dev \
+     gnutls-bin libgif-dev libxpm-dev \
      gnutls-dev libtinfo-dev libncurses-dev
-sudo apt install -y libgtk-3-dev # gtk-3
-sudo apt install -y libwebkit2gtk-4.0-dev # webkit
-sudo apt install -y libgccjit0 libgccjit-11-dev # native-compile
+sudo apt install -y \
+     libgccjit0 libgccjit-10-dev gcc-10 
+     # imagemagick libmagick++-dev \
+sudo apt install -y libxft-dev libxft2 # enable xft
+sudo apt install -y librsvg2-dev # enable rsvg2
+sudo apt install -y libwebp-dev webp # enable webp
+sudo apt install -y libjansson4 libjansson-dev # enable json
+sudo apt install -y libtree-sitter-dev # enable tree-sitter
+sudo apt install -y libgtk-3-dev # enable gtk-3
+sudo apt install -y libwebkit2gtk-4.0-dev # enable webkit
+sudo apt install -y libgccjit0 libgccjit-11-dev # enable native-compile
 configure="./configure --prefix=${PREFIX} ${OPTIONS}"
 echo $configure
 eval $configure
